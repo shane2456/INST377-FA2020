0aa677c3e0819ff93c612be1668ebb7b0476051e
shane2456 <shanevong@gmail.com>
2020-10-30 17:25:56 -0400

    Initial page

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // These are our required libraries to make the server work.
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-import express from 'express';
+import express, { json } from 'express';
 import dotenv from 'dotenv';
-import countries from './public/lab_6/countries.js'
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -23,12 +23,13 @@ app.use((req, res, next) => {
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
+    console.log('fetch request data', json);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
-    res.json(countries);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
+    const json = await data.json();
+    res.json(json);
   });
 
 app.listen(port, () => {
